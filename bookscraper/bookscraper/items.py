2414f096b99250484981b2268c85d36def84d0a4
marioazer <131077406+marioazer@users.noreply.github.com>
2025-05-29 14:08:04 -0400

    init commit

---
+++ bookscraper/bookscraper/items.py
@@ -0,0 +1,20 @@
+import scrapy
+
+# Optional serializer function (you can use this in pipelines if needed)
+def serialize_price(value):
+    return f'£ {str(value)}'
+
+class BookItem(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    upc = scrapy.Field()
+    product_type = scrapy.Field()
+    price_excl_tax = scrapy.Field()
+    price_incl_tax = scrapy.Field()
+    tax = scrapy.Field()
+    availability = scrapy.Field()
+    num_reviews = scrapy.Field()
+    stars = scrapy.Field()
+    category = scrapy.Field()
+    description = scrapy.Field()
+    price = scrapy.Field()
